58bf09d9e3cad538b5eb80bef70fd1e2d7f6d801
Andy Weiss <andyweiss1982@gmail.com>
2020-09-08 09:40:46 -0600

    reorder styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,15 @@ import styled from "styled-components";
 
 const Button = styled.button`
   border: 0;
+  cursor: pointer;
+  text-transform: uppercase;
+  color: white;
+  transition: background-color 0.3s;
+
   border-radius: ${(props) => (props.round ? "100%" : 0)};
   background-color: ${(props) => (props.primary ? "darkblue" : "deeppink")};
   box-shadow: ${(props) =>
     props.shadow && "0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.5)"};
-  color: white;
   font-size: ${(props) => (props.size === "large" ? "1.5rem" : undefined)};
   width: ${(props) =>
     props.fullWidth
@@ -22,12 +26,8 @@ const Button = styled.button`
       : props.size === "small"
       ? "1.75rem"
       : undefined};
-  cursor: pointer;
-  text-transform: uppercase;
   padding: ${(props) =>
     props.size === "small" || props.size === "large" ? undefined : "0.5rem"};
-
-  transition: background-color 0.3s;
   &:hover {
     background-color: ${(props) => props.primary && "dodgerblue"};
   }
