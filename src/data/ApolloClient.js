5e62e1eed82d5f2850055057f36228f173395e70
Lazaro <lquin008@gmail.com>
2020-09-17 00:45:11 -0400

    setup client in order to get jwt token for headers

--- src/data/ApolloClient.js
+++ src/data/ApolloClient.js
@@ -1,9 +1,28 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context"
+import Auth from "./Auth"
 import { TASKS_API_URL } from "./TasksApi";
 
+const httpLink = createHttpLink({uri: TASKS_API_URL})
+
+const authLink = setContext(async(_, { headers }) => {
+  let token
+  try{
+    const user = await Auth.currentAuthenticatedUser()
+    token = user.signInUserSession.accessToken.jwtToken
+    console.log({token})
+  } catch(error){
+    console.log(error.message)
+    token=""
+  }
+  return{
+    headers: {...headers, Authorization: token}
+  }
+})
+
 const client = new ApolloClient({
-  uri: TASKS_API_URL,
-  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 export default client;
